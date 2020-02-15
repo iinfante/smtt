0c216f75d3991a31868c8b24238f270cb3fe504e
Ismael Infante <iinfante@frigg.asgard.net>
2020-02-15 15:59:08 -0300

    Add structure for test suites.

---
+++ test/testsmtt.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(testsmttsuite)
+
+BOOST_AUTO_TEST_CASE(shouldDetectedDataField)
+{
+    BOOST_TEST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
